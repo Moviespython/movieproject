f67c7dd2e41c1a80504f7e8edd56f7c36b5e1045
Moviespython <111860798+Moviespython@users.noreply.github.com>
2022-08-23 15:45:39 +0530

    Delete movieproject directory

--- movieproject/movieapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.admin import views
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-   path('', views.index,name='index'),
-   path('movie/<int:movie_id>/',views.detail,name='detail')
-]
